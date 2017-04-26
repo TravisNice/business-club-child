ff3f057b28625dd930af84c0da94c770b7b71cd9
Travis Nice <travis.nice@everydaypublishing.com.au>
2017-04-26 15:10:24 +1000

    Superficial change for readability
    in the functions.php

--- functions.php
+++ functions.php
@@ -5,37 +5,24 @@
 function bcc_enqueue_styles () {
 
 	wp_enqueue_style (
-
 		'parent-style',
-
 		get_template_directory_uri () . '/style.css'
-
 	);
 
 	wp_enqueue_style (
-
 		'child-style',
-
 		get_stylesheet_directory_uri () . '/style.css',
-
 		array (
-
 			'parent-style'
-
 		),
-
 		wp_get_theme () -> get ( 'Version' )
-
 	);
 
 }
 
 add_action (
-
 	'wp_enqueue_scripts',
-
 	'bcc_enqueue_styles'
-
 );
 
 // Replace the Axel Themes spiel in their footer with a custom copyright notice for Goondiwindi Chamber of Commerce
@@ -49,35 +36,23 @@ function custom_copyright () {
 function custom_footer () {
 
 	remove_action (
-
 		'business_club_action_footer',
-
 		'business_club_footer_copyright',
-
 		10
-
 	);
 
 	add_action (
-
 		'business_club_action_footer',
-
 		'custom_copyright',
-
 		10
-
 	);
 
 }
 
 add_action (
-
 	'init',
-
 	'custom_footer',
-
 	10
-
 );
 
 /*
@@ -87,15 +62,25 @@ add_action (
 function gcc_mail_from ( $email ) {
 	return 'chamber@goondiwindi.qld.au';
 }
-add_filter ( 'wp_mail_from', 'gcc_mail_from' );
+	
+add_filter (
+	    'wp_mail_from',
+	    'gcc_mail_from'
+);
 
 /*
  * Change the From name in emails sent from the site
  */
 
 function gcc_mail_from_name ( $from_name ) {
+	
 	return 'Goondiwindi Chamber of Commerce';
+
 }
-add_filter ( 'wp_mail_from_name', 'gcc_mail_from_name' );
+
+add_filter (
+	    'wp_mail_from_name',
+	    'gcc_mail_from_name'
+);
 
 ?>
