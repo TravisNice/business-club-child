376085fdefb599c2b5450488f6830a04f5372081
Travis Nice <travis.nice@everydaypublishing.com.au>
2017-04-26 15:06:34 +1000

    Production version
    setup for the Goondiwindi Chamber of Commerce

---
+++ custom_copyright.php
@@ -0,0 +1,3 @@
+<div class="copyright">
+	Copyright &copy; <?php echo date ( 'Y' ); ?> Goondiwindi Chamber of Commerce | <a href="/privacy-policy">Privacy Policy</a> | <a rel="license" href="/terms-conditions">Terms &amp; Conditions</a><br /><span style="font-size: 80%">Website by <a href="https://www.everydaypublishing.com.au/" rel="external">Everyday Publishing</a></span>
+</div>
---
+++ functions.php
@@ -0,0 +1,101 @@
+<?php
+
+// Ensure the child themes style sheet gets loaded
+
+function bcc_enqueue_styles () {
+
+	wp_enqueue_style (
+
+		'parent-style',
+
+		get_template_directory_uri () . '/style.css'
+
+	);
+
+	wp_enqueue_style (
+
+		'child-style',
+
+		get_stylesheet_directory_uri () . '/style.css',
+
+		array (
+
+			'parent-style'
+
+		),
+
+		wp_get_theme () -> get ( 'Version' )
+
+	);
+
+}
+
+add_action (
+
+	'wp_enqueue_scripts',
+
+	'bcc_enqueue_styles'
+
+);
+
+// Replace the Axel Themes spiel in their footer with a custom copyright notice for Goondiwindi Chamber of Commerce
+
+function custom_copyright () {
+
+	require_once ( 'custom_copyright.php' );
+
+}
+
+function custom_footer () {
+
+	remove_action (
+
+		'business_club_action_footer',
+
+		'business_club_footer_copyright',
+
+		10
+
+	);
+
+	add_action (
+
+		'business_club_action_footer',
+
+		'custom_copyright',
+
+		10
+
+	);
+
+}
+
+add_action (
+
+	'init',
+
+	'custom_footer',
+
+	10
+
+);
+
+/*
+ * Change the email address the site sends from
+ */
+
+function gcc_mail_from ( $email ) {
+	return 'chamber@goondiwindi.qld.au';
+}
+add_filter ( 'wp_mail_from', 'gcc_mail_from' );
+
+/*
+ * Change the From name in emails sent from the site
+ */
+
+function gcc_mail_from_name ( $from_name ) {
+	return 'Goondiwindi Chamber of Commerce';
+}
+add_filter ( 'wp_mail_from_name', 'gcc_mail_from_name' );
+
+?>
